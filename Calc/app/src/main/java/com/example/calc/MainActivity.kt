ccc246430c6314be2f912b1eb456bf28539573ae
AdamSzr <adam.szr98@gmail.com>
2021-03-29 18:16:38 +0200

    Num-Pad working.

--- Calc/app/src/main/java/com/example/calc/MainActivity.kt
+++ Calc/app/src/main/java/com/example/calc/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberEvent(view: View) {
+        var editText =  findViewById<EditText>(R.id.editText)
+        var button_select = view as Button;
+        var boardNewText =  editText.text.toString().plus(button_select.text);
+        editText.setText( boardNewText )
     }
 }
